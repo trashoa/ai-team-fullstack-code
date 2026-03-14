b142d7ef9268c5c07f50f3bd11d2ff9e8420a4a1
root <root@localhost.localdomain>
2026-03-14 17:44:24 +0800

    test: 添加飞书通知集成测试文件

---
+++ test-notification.js
@@ -0,0 +1,11 @@
+/**
+ * 飞书通知集成测试文件
+ * 用于验证 PR 审查通知流程
+ */
+
+function testNotification() {
+  console.log('测试飞书通知集成');
+  return { success: true, message: '测试通过' };
+}
+
+module.exports = { testNotification };
